43a5dcf2bb662ded993cde8d647bece124480abe
sayedul alam <sayedul.alam@kcl.ac.uk>
2022-12-09 03:45:51 +0000

    solution solved

--- primes.py
+++ primes.py
@@ -3,4 +3,26 @@
 
 def primes(number_of_primes):
     list = []
+    if(number_of_primes <= 0):
+        raise ValueError
+
+    i = 2
+    if i == 2:
+        list.append(i)
+        i+=1
+    x = 1
+
+    while x <  number_of_primes:
+        for j in range(2, i):
+            if i%j==0:
+                break
+
+        if i == j+1:
+            list.append(i)
+            x+=1
+
+        i=i+1
     return list
+
+listy = primes(10)
+print(listy)
